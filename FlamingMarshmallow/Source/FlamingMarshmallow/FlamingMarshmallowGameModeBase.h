38f5201516a9644ed556206c218a6d4878c2bcbd
benr96 <C15507277@mydit.ie>
2017-02-21 18:18:05 +0000

    Unreal Source Test 1

---
+++ FlamingMarshmallow/Source/FlamingMarshmallow/FlamingMarshmallowGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "FlamingMarshmallowGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLAMINGMARSHMALLOW_API AFlamingMarshmallowGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	AFlamingMarshmallowGameModeBase();
+
+};
+
